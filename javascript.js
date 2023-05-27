45817730306afbbb17584f753dd0dd9481c4c490
GuoJing <guojing21@gmail.com>
2023-05-27 16:43:35 -0400

    .

--- javascript.js
+++ javascript.js
@@ -9,3 +9,37 @@ bar.addEventListener('click', () => {
 close.addEventListener('click', () => {
     navbar.classList.remove('active');
 })
+
+const productFaces = document.querySelectorAll('.small-face');
+const mainFace = document.querySelector('.main-face');
+productFaces.forEach(face => face.addEventListener('click', function() {
+    mainFace.src = face.src;
+}));
+
+var faceDetails = document.getElementById('face-details');
+var closePopupButtons = document.querySelectorAll('[data-close-button]');
+var overlay = document.getElementById('overlay');
+
+function openPopUp() {
+    faceDetails.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closePopUp() {
+    faceDetails.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+const seniors = document.querySelectorAll('.senior');
+seniors.forEach(senior => senior.addEventListener('click', openPopUp));
+
+closePopupButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        closePopUp();
+    })
+})
+
+overlay.addEventListener('click', () => {
+    closePopUp();
+})
+
